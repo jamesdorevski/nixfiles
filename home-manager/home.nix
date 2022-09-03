384486ad2b3824dbce6beef2e3f70abb174810b1
jamesdorevski <james@jamesdorevski.com>
2022-09-03 15:29:12 +1000

    chore: readded home

---
+++ home-manager/home.nix
@@ -0,0 +1,24 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "james";
+  home.homeDirectory = "/home/james";
+
+  home.file.".vimrc".source = ./vimrc;
+
+  programs.git = {
+    enable = true;
+    userName = "jamesdorevski";
+    userEmail = "james@jamesdorevski.com";
+    aliases = {
+      st = "status";
+      cm = "commit";
+      pu = "push";
+      pl = "pull";
+      aa = "add .";
+    };
+  };
+
+  home.stateVersion = "22.05";
+  programs.home-manager.enable = true;
+}
