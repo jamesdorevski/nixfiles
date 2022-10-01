5c7e1c8c22f6cb797be397341a764baa9702928d
jamesdorevski <james@jamesdorevski.com>
2022-10-01 16:38:04 +1000

    chore: added nvim

--- configuration.nix
+++ configuration.nix
@@ -1,5 +1,4 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
+# Edit this configuration file to define what should be installed on # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
 { config, pkgs, ... }:
@@ -82,7 +81,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    helix
+    neovim
     spotify
     firefox
     vscode
